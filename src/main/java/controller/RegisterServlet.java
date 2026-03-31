3152ccd7a569f5cd3cd255a45561e3977995e814
sanjay020904 <s.sanjay.aiml@gmail.com>
2026-03-31 15:16:00 +0530

    CART system Commit

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 @WebServlet("/Register")
 public class RegisterServlet extends HttpServlet {
